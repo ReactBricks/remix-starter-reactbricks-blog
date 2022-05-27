3518e62ed4f14a5803a33e7463ac7faadd0877cd
Giorgio Tognoli <giorgio@f2.net>
2022-05-27 17:59:43 +0200

    updated react-bricks-ui and add interface to layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,7 +1,12 @@
 import Header from './Header'
 import Footer from './Footer'
+import { ReactNode } from 'react'
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  children?: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen justify-between font-content antialiased">
       <Header />
